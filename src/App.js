84fdc427df2c420de0825e89fcb3bb12220df813
DeveloperIsh1179 <ismael.sanchez.1179@gmail.com>
2023-02-19 03:11:11 -0800

    completed the button implementation of the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from './Button';
+
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <div>
+        <Button primary rounded>
+          <GoBell />
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button success rounded outline>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button warning>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button danger>Something!</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
